02c5d3880266d5f9917e5995f5061eac841a88ac
pratham2707 <116713160+pratham2707@users.noreply.github.com>
2023-01-17 13:52:16 +0530

    Add files via upload
    Wrapper Class in Java.

---
+++ WC1.java
@@ -0,0 +1,16 @@
+class WC1{
+
+public static void main(String[] arg){
+
+Integer a = Integer.valueOf(1);
+Double b = Double.valueOf(5.55);
+
+int c = a.intValue();
+double d = b.doubleValue();
+
+System.out.println(c);
+System.out.println(d);
+
+}
+
+}
