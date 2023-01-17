429b77a3b41df37c1e7e87e5ab1bef2d5fc3db50
pratham2707 <116713160+pratham2707@users.noreply.github.com>
2023-01-17 13:51:37 +0530

    Add files via upload
    Wrapper Class in Java.

---
+++ WC.java
@@ -0,0 +1,20 @@
+class WC {
+  public static void main(String[] arg) {
+
+    // create primitive types
+    int a = 5;
+    double b = 5.65;
+
+    //converts into wrapper objects
+    Integer aObj = Integer.valueOf(a);
+    Double bObj = Double.valueOf(b);
+
+    if(aObj instanceof Integer) {
+      System.out.println("An object of Integer is created.");
+    }
+
+    if(bObj instanceof Double) {
+      System.out.println("An object of Double is created.");
+    }
+  }
+}
